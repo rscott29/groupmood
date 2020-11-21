956f81577d42cb077fcf14ad341815af95e6c495
rich <r.k.scott@outlook.com>
2020-11-21 17:12:12 +0000

    lazy load layout module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+  ],
+  exports: [
+    CommonModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+  ]
+})
+export class SharedModule { }
